de89358a89239f5737251702aceb178804972b10
Your Name <you@example.com>
2024-11-04 08:35:44 +0100

    elso

---
+++ Projekt2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Projekt2
+{
+    internal class Program
+    {
+        static void Beiras()
+        {
+
+        }
+        static void Adatolvasas()
+        {
+            StreamReader sr = new StreamReader("Adatok.txt");
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
