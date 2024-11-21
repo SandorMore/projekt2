041acd9fc45852829cdfcacaff5c5500a4d8e260
MideczkiAdam <116632287+MideczkiAdam@users.noreply.github.com>
2024-11-21 11:14:44 +0100

    gombok kesz, kosarhoz localstorage

--- ikt2/website.js
+++ ikt2/website.js
@@ -1,25 +1,130 @@
-// TODO ha nincs kivalasztva semmi
-function myFunction(kivalhaz) {
-    // Get the snackbar DIV
-    var x = document.getElementById("snackbar");
-    
-    x.className = "show";
-    
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-    
-    // Add the "show" class to DIV
-}
+var tartalmazza = document.getElementById("snackbar")
+var nemtartalmazza = document.getElementById("snackbar2")
 
 
 let kivallap = document.getElementById("valasztottlap")
 let lapok = document.querySelectorAll(".lap")
 
-lapok.forEach(hazfunc)
-
-function hazfunc(lap){
+lapok.forEach((lap)=>{
     lap.addEventListener("click", ()=>{
         kivallap.innerHTML = lap.innerHTML
         kivallap.classList.add("kivalszin")
     })
-}
+})
+
+let lapAdd = document.getElementById("lapAdd")
+lapAdd.addEventListener("click", ()=>{
+    if (kivallap.classList.contains("kivalszin")){
+        tartalmazza.className = "show";
+        setTimeout(function(){ tartalmazza.className = tartalmazza.className.replace("show", ""); }, 3000);
+    }
+    else{
+        nemtartalmazza.className = "show";
+        setTimeout(function(){ nemtartalmazza.className = nemtartalmazza.className.replace("show", ""); }, 3000);
+    }
+})
+
+let kivalcpu = document.getElementById("valasztottcpu")
+let cpuk = document.querySelectorAll(".cpu")
+
+cpuk.forEach((cpu)=>{
+    cpu.addEventListener("click", ()=>{
+        kivalcpu.innerHTML = cpu.innerHTML
+        kivalcpu.classList.add("kivalszin")
+    })
+})
+
+let kivaltap = document.getElementById("valasztotttap")
+let tapok = document.querySelectorAll(".tap")
+
+tapok.forEach((tap)=>{
+    tap.addEventListener("click", ()=>{
+        kivaltap.innerHTML = tap.innerHTML
+        kivaltap.classList.add("kivalszin")
+    })
+})
+
+let kivalgpu = document.getElementById("valasztottgpu")
+let gpuk = document.querySelectorAll(".gpu")
+
+
+gpuk.forEach((gpu)=>{
+    gpu.addEventListener("click", ()=>{
+        kivalgpu.innerHTML = gpu.innerHTML
+        kivalgpu.classList.add("kivalszin")
+    })
+})
+
+let kivalram = document.getElementById("valasztottram")
+let ramok = document.querySelectorAll(".ram")
+
+ramok.forEach((ram)=>{
+    ram.addEventListener("click", ()=>{
+        kivalram.innerHTML = ram.innerHTML
+        kivalram.classList.add("kivalszin")
+    })
+})
+
+let kivalmemory = document.getElementById("valasztottmemory")
+let memoryk = document.querySelectorAll(".memory")
+
+memoryk.forEach((memory)=>{
+    memory.addEventListener("click", ()=>{
+        kivalmemory.innerHTML = memory.innerHTML
+        kivalmemory.classList.add("kivalszin")
+    })
+})
+
+let kivalmonitor = document.getElementById("valasztottmonitor")
+let monitorok = document.querySelectorAll(".monitor")
+let nomonitor = document.getElementById("nomonitor")
+
+nomonitor.addEventListener("click", ()=>{
+    nomonitor.classList.add("kivalasztottnemgomb")
+    kivalmonitor.innerHTML = null
+})
+
+monitorok.forEach((monitor)=>{
+    monitor.addEventListener("click", ()=>{
+        nomonitor.classList.remove("kivalasztottnemgomb")
+        kivalmonitor.innerHTML = monitor.innerHTML
+        kivalmonitor.classList.add("kivalszin")
+    })
+})
+
+let kivalbill = document.getElementById("valasztottbill")
+let billek = document.querySelectorAll(".bill")
+let nobill = document.getElementById("nobill")
+
+nobill.addEventListener("click", ()=>{
+    nobill.classList.add("kivalasztottnemgomb")
+    kivalbill.innerHTML = null
+})
+
+billek.forEach((bill)=>{
+    bill.addEventListener("click", ()=>{
+        nobill.classList.remove("kivalasztottnemgomb")
+        kivalbill.innerHTML = bill.innerHTML
+        kivalbill.classList.add("kivalszin")
+    })
+})
+
+let kivaleger = document.getElementById("valasztotteger")
+let egerek = document.querySelectorAll(".eger")
+let noeger = document.getElementById("noeger")
+
+noeger.addEventListener("click", ()=>{
+    noeger.classList.add("kivalasztottnemgomb")
+    kivaleger.innerHTML = null
+})
+
+egerek.forEach((eger)=>{
+    eger.addEventListener("click", ()=>{
+        noeger.classList.remove("kivalasztottnemgomb")
+        kivaleger.innerHTML = eger.innerHTML
+        kivaleger.classList.add("kivalszin")
+    })
+})
+
+
+
