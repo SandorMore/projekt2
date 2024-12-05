f064a2435f382e7eefbfab0b6a4eec916db9dbc2
RidegCsaba <ridegslug@gmail.com>
2024-12-05 09:19:11 +0100

    txt_converter

--- txt_converter.py
+++ txt_converter.py
@@ -7,11 +7,19 @@ for count,line in enumerate(lines):
     line = line.strip().split(";")
     line[2] = line[2].split(",")
     specs = {}
+    print(count)
     for count, i in enumerate(line[2]):
-        i.split(":")
-        specs.append(i[0]:i[1])
+        parts = i.split(":")
+        print(parts)
+        print(parts[0],parts[1])
+        specs[parts[0]] = parts[1]
 
-    data.append({"type":line[0],"name":line[1],"specs":line[2],"price":line[3]})
+    data.append({"type":line[0],"name":line[1],"specs":specs,"price":line[3]})
 
 for i in data:
     print(i)
+
+json_object = json.dumps(data, ensure_ascii=False ,indent=4)
+
+with open("components.json", "w",encoding="utf8") as outfile:
+    outfile.write(json_object)
